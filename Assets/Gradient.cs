a1777c3cb52bfb7fa379998b7fbbed962543ddeb
Samuel Cawley <samrcawley@gmail.com>
2014-05-29 14:46:10 -0400

    Begin of unity port

---
+++ Assets/Gradient.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gradient : MonoBehaviour {
+
+	private Color edgeColor = new Color (255, 255, 255);
+	private Color midColor = new Color(200, 200, 255);
+	// Use this for initialization
+	void Start () {
+		Mesh bg = ((MeshFilter)GetComponent(typeof(MeshFilter))).mesh;
+		Color[] colors = new Color[bg.vertices.Length];
+		colors[0] = edgeColor;
+		colors[1] = edgeColor;
+		colors[2] = midColor;
+		colors[3] = midColor;
+		bg.colors = colors;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/NekoMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NekoMovement : MonoBehaviour {
+
+	public float maxVelocity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePosition = Input.mousePosition;
+		
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		
+		//transform.position = Vector2.Lerp(transform.position, mousePosition, maxVelocity);
+
+		rigidbody2D.velocity = Vector2.ClampMagnitude (new Vector2 (mousePosition.x - transform.position.x, mousePosition.y - transform.position.y)*10, maxVelocity);
+
+	}
+}
